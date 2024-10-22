7aa5ec86f2ff239f6669f3be2c4882e572b7a437
Alex Ramos <alexplayerramostola54@gmail.com>
2024-10-22 10:52:37 -0400

    carrito1

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask,render_template,request,session,redirect,url_for
+
+app=Flask(__name__)
+
+
+@app.route("/")
+def carrito():
+    if 'lista' not in session:
+        session['lista']=[]
+    return render_template('index.html',lista=session['lista'])
+
+@app.route("/procesa",methods=['GET','POST'])
+def procesa():
+    producto=request.form.get("producto")
+    if 'lista' in session and producto:
+        session['lista'].append(producto)
+        session.modified=True
+    return redirect(url_for("carito"))    
+@app.route("/vaciar",methods=["GET"])
+def vaciar():
+    session.pop("lista",None)
+    return redirect(url_for("carrito"))
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+
